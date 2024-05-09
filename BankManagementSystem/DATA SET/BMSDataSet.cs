c34f92d5af6b81e767961aa9af192e23e5c2111e
Hammad Shabir <chhammadsahbir@gmail.com>
2024-05-09 04:07:26 +0100

    Code Update

---
+++ BankManagementSystem/DATA SET/BMSDataSet.cs
@@ -0,0 +1,14 @@
+﻿namespace BankManagementSystem.DATA_SET {
+    
+    
+    public partial class BMSDataSet {
+    }
+}
+
+namespace BankManagementSystem.DATA_SET.BMSDataSetTableAdapters
+{
+    
+    
+    public partial class GetAccountSummaryTableAdapter {
+    }
+}
